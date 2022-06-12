ac7a377bb1b9caaf233cf689810e137f0930829b
walysonfelipe <walysonfelipe25@gmail.com>
2022-06-11 22:44:08 -0300

    CorreiosTracker- adicionando features novas

--- app/services/stages/tracker.ts
+++ app/services/stages/tracker.ts
@@ -3,40 +3,40 @@ import correiosTracker from '../tracker/correios'
 import { User } from '../../models/userModel'
 import { Correios } from '../../models/correiosModel'
 async function execute(user: User, code: string) {
-  const messagesToSend: any = []
-     
-    if(code.length != 13){
-     messagesToSend.push('❌ *Código inválido* , Digite o Código de Rastreio novamente!!') 
-     return messagesToSend  
-    }
+    const messagesToSend: any = []
 
-    messagesToSend.push('*📦 Sua Encomenda *'+ code +'*, Veja aqui  abaixo 👇🏾') 
-     await correiosTracker(code).then((trackedParcel:any)=>{
-            
+    let status = 0
+
+    if (code.length != 13)
+        status = 1
+
+
+    await correiosTracker(code).then((trackedParcel: any) => {
+        status = trackedParcel[0].length == 0 ? 1 : status
+
+        if (status == 0) {
+            messagesToSend.push('📦 Sua Encomenda *' + code + '*, Veja aqui  abaixo 👇🏾')
             trackedParcel[0].forEach((parcelPoint: Correios) => {
-            if(parcelPoint.local){
-              messagesToSend.push('✅ *STATUS:* *_'+ parcelPoint.status +'_*\n\n 📅 *DATA:* '+ parcelPoint.data +' - ' + parcelPoint.hora +'\n\n *📬 LOCAL:* ' + parcelPoint.local);
-            }
-            else{
-                messagesToSend.push('✅ *STATUS:* *_'+ parcelPoint.status +'_*\n\n 📅 *DATA:* '+ parcelPoint.data +' - ' + parcelPoint.hora +'\n\n *🏙️ ORIGEM:* ' + parcelPoint.origem +'*\n\n *🚚 DESTINO:* ' + parcelPoint.destino);  
-            }
+                if (parcelPoint.local) {
+                    messagesToSend.push('✅ *STATUS:* *_' + parcelPoint.status + '_*\n\n 📅 *DATA:* ' + parcelPoint.data + ' - ' + parcelPoint.hora + '\n\n *📬 LOCAL:* ' + parcelPoint.local);
+                }
+                else {
+                    messagesToSend.push('✅ *STATUS:* *_' + parcelPoint.status + '_*\n\n 📅 *DATA:* ' + parcelPoint.data + ' - ' + parcelPoint.hora + '\n\n *🏙️ ORIGEM:* ' + parcelPoint.origem + '\n\n *🚚 DESTINO:* ' + parcelPoint.destino);
+                }
             });
+
+            messagesToSend.push('Muito obrigado por utilizar nossa solução.. 🥰🥰🥰\n\n *Quer rastrear outra encomenda 📦? *');
+        }
+        else{
+            messagesToSend.push('❌ *Código inválido* , Digite o Código de Rastreio novamente!!') 
+        }
+
     });
-     
-   // code
-  // buscar codigo 
 
-// montar layout da mensagem
-
-
-
-//   changeUser(user.from, 'stage', 2)
-//   messagesToSend.push('Olá meu nome é Rastry🙋🏾‍♂️, sou fascinado por rastrear encomendas📦')
-//   messagesToSend.push('Quer saber aonde está sua encomenda ? !\n\n *📦 Digite o codigo de rastreio...*')
-  return messagesToSend
+    return messagesToSend
 }
 
 
 export {
-  execute
+    execute
 }
